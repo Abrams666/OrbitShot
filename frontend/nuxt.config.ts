7ffab37b31750cf24b51186f9dd507fe144068f3
Abrams <yuanchianchen@gmail.com>
2025-11-12 11:55:33 +0800

    add: add icon, title and update color style

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,4 +3,10 @@ export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
 	devtools: { enabled: true },
 	css: ["~/assets/css/css.css"],
+	app: {
+		head: {
+			title: "OrbitShot",
+			meta: [{ name: "description", content: "高解析太空影像訂製服務" }],
+		},
+	},
 });
