4a09efe15a5824b0431ee9337dc0491df058fe2e
Abrams <yuanchianchen@gmail.com>
2025-11-19 01:56:11 +0800

    chore: google verification

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -6,7 +6,10 @@ export default defineNuxtConfig({
 	app: {
 		head: {
 			title: "OrbitShot",
-			meta: [{ name: "description", content: "高解析太空影像訂製服務" }],
+			meta: [
+				{ name: "description", content: "高解析太空影像訂製服務" },
+				{ name: "google-site-verification", content: "flD5G-N7i0uH2i0uH2i41RYc5xMR77g_zwaD8kPdFdjCZ_1Q" },
+			],
 		},
 	},
 	runtimeConfig: {
